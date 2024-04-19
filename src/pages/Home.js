aa710972204fa0cd7efd0df04492783fbe5f30d1
Indrajit <indrajitchandra98@gmail.com>
2024-04-19 11:38:37 +0600

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+
+  const[products,setProducts] = useState([])
+  console.log(products);
+  useEffect(()=> {
+    fetch('products.json')
+    .then(response=>response.json())
+    .then(data=>setProducts(data));
+
+  },[]);
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {
+        products.map(product => <ProductCard product={product} key={product.price}></ProductCard>)
+      }
+    </div>
+  );
+};
+
+export default Home;
